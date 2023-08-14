a3e6047746051c77b0418c1349263475a305f81b
tsao <nickregankitsao@gmail.com>
2023-08-15 01:46:40 +0300

    printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main - printing a line made up of an array of characters using put function
+ *Mystr: string initialisation
+ *Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char Mystr[] = "\"Programming is like building a multilingual";
+
+	puts(Mystr);
+
+	return (0);
+}
